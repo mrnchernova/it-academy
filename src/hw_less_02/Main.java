798c6aa7de280775f4778e7d6838dc09dc12739c
mchernova <mchernova3706@gmail.com>
2023-06-19 01:15:46 +0300

    Домашнее задание "Типы данных"

---
+++ src/hw_less_02/Main.java
@@ -0,0 +1,99 @@
+package hw_less_02;
+
+/**
+ * 1. Создать два класса.
+ * 2. Создать метод для запуска в одном классе. (метод main)
+ * 3. Во втором классе создать глобальные переменные всех типов (примитивы и обертки).
+ * 3.1 И для каждой глобальной переменной геттеры и сеттеры.
+ * 4. В классе с методом для запуска создать глобальные переменные с различными типами (примитивами и обертками).
+ * А также в методе (main) создать локальные переменные (примитивы и обертки).
+ * 5. Поэкспериментировать с присвоением переменных друг в друга и приведением типов, посмотреть какие ошибки получаться.
+ * 6. Геттеры и сеттеры обязательно использовать, т.е. установить и получить значение.
+ * 7. Вывести все переменные на консоль. (вывод на экран)
+ * 8. Создавать классы, методы, переменные со всеми правилами именования. (code convention)
+ */
+
+public class Main {
+    public static byte simpleByte = 100;
+    public static int simpleInt = 100;
+    public static long simpleLong = 100L;
+    public static float simpleFloat = 100.1F;
+    public static boolean simpleBoolean = true;
+    public static char simpleChar = 'A';
+    public static String simpleString = "global string";
+
+    public static Integer wrapperInteger = 200;
+    public static Long wrapperLong = 200L;
+    public static Double wrapperDouble = 200.20;
+    public static Boolean wrapperBoolean = false;
+    public static Character wrapperCharacter = 'W';
+
+    public static void main(String[] args) {
+
+        byte localSimpleByte = 100;
+        int localSimpleInt = 1;
+        long localSimpleLong = 1L;
+        double localSimpleDouble = 1.3;
+        boolean localSimpleBoolean = true;
+        char localSimpleChar = 'c';
+        String localSimpleString = "local string";
+
+
+        Integer localWrapperInteger = 35;
+        Long localWrapperLong = 2L;
+        Float localWrapperFloat = 2.2F;
+        Boolean localWrapperBoolean = true;
+        Character localWrapperCharacter = 'w';
+
+
+        System.out.println("\nЦелочисленные переменные");
+        var x = 0;
+        x = (byte) (localSimpleByte + localWrapperInteger); //100 + 35 = -121. Результат вышел за пределы диапазона byte
+        System.out.println("byte 100 + 35 = " + x);
+
+        x = localSimpleByte + localWrapperInteger; //100 + 35 = 135. Получен результат типа int
+        System.out.println("Integer 100 + 35 = " + x);
+
+        SecondClass secondClass = new SecondClass();
+        secondClass.setSimpleByte((byte) 3);
+        secondClass.setWrapperFloat(2.3F);
+        System.out.println("getter and setter: " + (secondClass.getSimpleByte() + secondClass.getWrapperFloat()));
+
+        int i = (int) (localSimpleLong + simpleInt); //нужно привести выражение к типу int
+        long l = localSimpleLong + simpleInt;        //не нужно ничего приводить
+        System.out.println("i = " + i + "\nl = " + l);
+
+
+        System.out.println("\nВещественные переменные");
+        float f = (float) (simpleFloat + localSimpleDouble);
+        System.out.println("f = " + f);
+
+        double d = simpleFloat + localSimpleDouble;
+        System.out.println("d = " + d);
+
+        System.out.println("\nСложение целочисленных и вещественных переменных");
+        secondClass.setSimpleLong(100L);
+        secondClass.setWrapperDouble(0.500);
+        long l1 = (long) (localSimpleInt + secondClass.getWrapperDouble());
+        System.out.println("long l1 = " + l1); //получим 1 т.к. приводим к целочисленному типу
+        Double d1 = localSimpleInt + secondClass.getWrapperDouble();
+        System.out.println("Double d1 = " + d1); // по умолчанию выражение будет типа double
+
+        System.out.println("\nСтроки и символы");
+        secondClass.setSimpleChar('Y');
+        simpleChar = secondClass.getSimpleChar();
+        System.out.println("Получаем и выводим символ из другого класса: " + secondClass.getSimpleChar());
+
+        secondClass.setSimpleString("example");
+        simpleString = localSimpleString + " --- " + secondClass.getSimpleString();
+        System.out.println("Сложение строк: " + simpleString);
+
+//        simpleChar = secondClass.getSimpleString();  //Ошибка! Нельзя присвоить типу char целую строку
+
+        secondClass.setSimpleString(String.valueOf(simpleChar)); //можно присвоить символ строке
+        System.out.println("Строка \"example\" заменена на \"" + secondClass.getSimpleString() + "\"");
+
+
+
+    }
+}
---
+++ src/hw_less_02/SecondClass.java
@@ -0,0 +1,159 @@
+package hw_less_02;
+
+public class SecondClass {
+    public byte simpleByte;
+    public short simpleShort;
+    public int simpleInt;
+    public long simpleLong;
+    public float simpleFloat;
+    public double simpleDouble;
+    public boolean simpleBoolean;
+    public char simpleChar;
+    public String simpleString;
+
+    public Byte wrapperByte;
+    public Short wrapperShort;
+    public Integer wrapperInteger;
+    public Long wrapperLong;
+    public Float wrapperFloat;
+    public Double wrapperDouble;
+    public Boolean wrapperBoolean;
+    public Character wrapperCharacter;
+
+    public byte getSimpleByte() {
+        return simpleByte;
+    }
+
+    public void setSimpleByte(byte simpleByte) {
+        this.simpleByte = simpleByte;
+    }
+
+    public short getSimpleShort() {
+        return simpleShort;
+    }
+
+    public void setSimpleShort(short simpleShort) {
+        this.simpleShort = simpleShort;
+    }
+
+    public int getSimpleInt() {
+        return simpleInt;
+    }
+
+    public void setSimpleInt(int simpleInt) {
+        this.simpleInt = simpleInt;
+    }
+
+    public long getSimpleLong() {
+        return simpleLong;
+    }
+
+    public void setSimpleLong(long simpleLong) {
+        this.simpleLong = simpleLong;
+    }
+
+    public float getSimpleFloat() {
+        return simpleFloat;
+    }
+
+    public void setSimpleFloat(float simpleFloat) {
+        this.simpleFloat = simpleFloat;
+    }
+
+    public double getSimpleDouble() {
+        return simpleDouble;
+    }
+
+    public void setSimpleDouble(double simpleDouble) {
+        this.simpleDouble = simpleDouble;
+    }
+
+    public boolean isSimpleBoolean() {
+        return simpleBoolean;
+    }
+
+    public void setSimpleBoolean(boolean simpleBoolean) {
+        this.simpleBoolean = simpleBoolean;
+    }
+
+    public char getSimpleChar() {
+        return simpleChar;
+    }
+
+    public void setSimpleChar(char simpleChar) {
+        this.simpleChar = simpleChar;
+    }
+
+    public String getSimpleString() {
+        return simpleString;
+    }
+
+    public void setSimpleString(String simpleString) {
+        this.simpleString = simpleString;
+    }
+
+    public Byte getWrapperByte() {
+        return wrapperByte;
+    }
+
+    public void setWrapperByte(Byte wrapperByte) {
+        this.wrapperByte = wrapperByte;
+    }
+
+    public Short getWrapperShort() {
+        return wrapperShort;
+    }
+
+    public void setWrapperShort(Short wrapperShort) {
+        this.wrapperShort = wrapperShort;
+    }
+
+    public Integer getWrapperInteger() {
+        return wrapperInteger;
+    }
+
+    public void setWrapperInteger(Integer wrapperInteger) {
+        this.wrapperInteger = wrapperInteger;
+    }
+
+    public Long getWrapperLong() {
+        return wrapperLong;
+    }
+
+    public void setWrapperLong(Long wrapperLong) {
+        this.wrapperLong = wrapperLong;
+    }
+
+    public Float getWrapperFloat() {
+        return wrapperFloat;
+    }
+
+    public void setWrapperFloat(Float wrapperFloat) {
+        this.wrapperFloat = wrapperFloat;
+    }
+
+    public Double getWrapperDouble() {
+        return wrapperDouble;
+    }
+
+    public void setWrapperDouble(Double wrapperDouble) {
+        this.wrapperDouble = wrapperDouble;
+    }
+
+    public Boolean getWrapperBoolean() {
+        return wrapperBoolean;
+    }
+
+    public void setWrapperBoolean(Boolean wrapperBoolean) {
+        this.wrapperBoolean = wrapperBoolean;
+    }
+
+    public Character getWrapperCharacter() {
+        return wrapperCharacter;
+    }
+
+    public void setWrapperCharacter(Character wrapperCharacter) {
+        this.wrapperCharacter = wrapperCharacter;
+    }
+
+}
