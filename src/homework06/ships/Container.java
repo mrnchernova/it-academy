d4e06b22bdc81d532baad8ad9b803e9816a3c83d
mrchernova <cherraniram@gmail.com>
2023-07-14 16:01:57 +0300

    Создание контейнера с рандомными параметрами и расчет его массы и объема

--- src/homework06/ships/Container.java
+++ src/homework06/ships/Container.java
@@ -1,18 +1,22 @@
 package homework06.ships;
 
+import java.text.DecimalFormat;
 import java.util.Arrays;
 
 public class Container {
     private int[] diagonal = new int[]{10, 20};
-    private int[] density = new int[]{1000, 2000}; //plotnost'
+    public int[] density = new int[]{1000, 2000}; //plotnost'
     private int height; //[10..100]
-    private String[] shape = new String[]{"square", "cylinder", "cone"};
-    private String[] size = new String[]{"big", "small"};
+    public String[] shape = new String[]{"square", "cylinder", "cone"};
+    public String[] size = new String[]{"big", "small"};
 
 
     public void getContainerWeight(String shape, String size, int height, int p) {
+        DecimalFormat decimalFormat = new DecimalFormat("#.###");
+        double V;
         double m;
         int diagonal;
+
         if (size.equals("big")) {
             diagonal = 20;
         } else {
@@ -21,19 +25,42 @@ public class Container {
 
         switch (shape) {
             case "square":
+
+                System.out.println("plotn " + p);
+                System.out.println("height " + height);
+                
+                double c;
+                c = Math.sqrt(Math.pow(diagonal / 2, 2) + Math.pow(diagonal / 2, 2));
+                V = c * c * height;
+                System.out.println("V = " + decimalFormat.format(V));
+                m = V * p;
+                System.out.println("Weight of " + size + " " + shape + " container = " + decimalFormat.format(m) + " ed\n");
                 break;
-            case "cylinder": //https://www.stresscalc.ru/volume/v1.php
-                double V = Math.PI * (Math.pow(diagonal, 2) / 4) * height;  //V=(π*d2/4)h
-                System.out.println("V = " + V);
-                m = V * p/(Math.pow(1000,3));
-                System.out.println("Weight of " + size + " " + shape + " container = " + m + " kg");
+
+            case "cylinder":
+
+                System.out.println("plotn " + p);
+                System.out.println("height " + height);
+                
+                V = Math.PI * (Math.pow(diagonal, 2) / 4) * height;
+                System.out.println("V = " + decimalFormat.format(V));
+                m = V * p;
+                System.out.println("Weight of " + size + " " + shape + " container = " + decimalFormat.format(m) + " ed\n");
                 break;
+
             case "cone":
+
+                System.out.println("water density " + p);
+                System.out.println("container height " + height);
+                
+                V = (Math.PI * Math.pow(diagonal / 2, 2) * height) / 3;
+                System.out.println("V = " + decimalFormat.format(V));
+                m = V * p;
+                System.out.println("Weight of " + size + " " + shape + " container = " + decimalFormat.format(m) + " ed\n");
                 break;
         }
-
-
     }
+
 
     public Container() {
     }
--- src/homework06/ships/Main.java
+++ src/homework06/ships/Main.java
@@ -1,5 +1,9 @@
 package homework06.ships;
 
+import org.w3c.dom.ls.LSOutput;
+
+import java.util.Random;
+
 /**
  * Построить иерархию.
  * 0.   Обязательные сущности (порт, корабль, палуба, контейнер)
@@ -16,11 +20,26 @@ package homework06.ships;
  * Для определения размера контейнера использовать флаг или писать сущности . У корабля должно быть имя. Удаление по имени.
  */
 public class Main {
+    private static Random random = new Random();
+
     public static void main(String[] args) {
         OneDeckShip sop = new OneDeckShip();
         sop.chargeShip();
         Container c = new Container();
-        c.getContainerWeight("cylinder","big", 40, 2000);
 
+        c.getContainerWeight("square", "big", 40, 2000);
+
+        System.out.println();
+
+        int randomShape = random.nextInt(c.shape.length);
+        int randomSize = random.nextInt(c.size.length);
+        int randomDensity = random.nextInt(c.density.length);
+        c.getContainerWeight(c.shape[randomShape], c.size[randomSize], (int) (Math.random() * 91) + 10, c.density[randomDensity]);
+        c.getContainerWeight(c.shape[randomShape], c.size[randomSize], (int) (Math.random() * 91) + 10, c.density[randomDensity]);
+        c.getContainerWeight(c.shape[randomShape], c.size[randomSize], (int) (Math.random() * 91) + 10, c.density[randomDensity]);
+        c.getContainerWeight(c.shape[randomShape], c.size[randomSize], (int) (Math.random() * 91) + 10, c.density[randomDensity]);
+
+//     for (int i=0; i<50;i++)
+//        System.out.println((int) (Math.random() * 91) + 10);
     }
 }
